66ad1ac4e34303c448d5977914bfed38608b52b9
“Divya <divyasajjan@yahoo.co.in>
2025-12-25 19:42:07 -0500

    Add Product serializer with controlled field exposure

--- products/serializers.py
+++ products/serializers.py
@@ -4,4 +4,14 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'id',
+            'product_id',
+            'product_category',
+            'product_price',
+            'product_manufacturing_date',
+            'product_expiry_date',
+            'supplier_cost',
+            'internal_notes',
+        ]
+        read_only_fields = ['supplier_cost', 'internal_notes']
